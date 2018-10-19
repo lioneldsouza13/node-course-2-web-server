c8ba82123e99ec52f1c675d399634d093e1a690b
Lionel D'souza <lioneldsouza51@gmail.com>
2018-10-19 20:58:44 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express =require('express');
+const hbs = require('hbs');
+var app = express();
+const fs =require('fs');
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+
+    var log =`${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFileSync('server.log',log +'\n');
+
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs',{
+//         pageTitle:"Maintenance",
+//         welcomeMessage:"Maintenance Underway"
+//     })
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>
+{
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage: 'Hello User ... Welcome to my Page'
+
+    });
+
+});
+
+app.get('/about',(req,res)=>
+{
+   res.render('about.hbs',{
+       pageTitle:'About Page',
+
+   });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        error: 'Unable to receive data'
+    })
+})
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+});
